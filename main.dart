2a1122f7ef7c3b4347c1851ee2efbb4a854c494d
Achmad <achmadsyarif2002@gmail.com>
2021-02-02 08:45:04 +0700

    tambah main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+class HomeApp extends StatefulWidget {
+  HomeApp({Key key}) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
